2b2a1e99a78295921ab7bc310a5fcb25da0673c2
dwleonhardt <dwleonhardt@gmail.com>
2018-02-08 14:45:46 -0700

    add send message from intercom

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }))
+const Intercom = require('intercom-client');
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
 const port = process.env.PORT || 3000;
 const accountSid = process.env.sid;
-const authToken = process.env.token;
+const twilioToken = process.env.twilio_token;
+const client = new Intercom.Client({ token: process.env.intercom_token });
 
 
+app.use(bodyParser.json({ extended: true }));
 
 app.listen(port, () => {
   console.log('Listening on port', port);
@@ -18,17 +20,22 @@ app.listen(port, () => {
 app.use(express.static('public'));
 
 app.post('/', function (req, res) {
-  console.log(req.body);
-  let zip = req.body.FromZip;
-  let state = req.body.State;
-  let phone = req.body.From;
-  let message = req.body.Body;
-  var twilio = require('twilio');
-  var client = new twilio(accountSid, authToken);
-  client.messages.create({
-    body: 'Thanks For Your Message',
-    to: `${phone}`,  // Text this number
-    from: '+17206054564' // From a valid Twilio number
-  })
-  .then((message) => console.log(message.sid));
+
+  let message = req.body.data.item.conversation_parts.conversation_parts[0].body.replace(/<(?:.|\n)*?>/gm, '');
+
+  let userId = req.body.data.item.user.id;
+
+
+  client.users.find({ id: `${userId}` }, (user) => {
+    var phone = user.body.phone;
+    var twilio = require('twilio');
+    var client = new twilio(accountSid, twilioToken);
+    client.messages.create({
+      body: `${message}`,
+      to: `${phone}`,
+      from: '+17206054564' // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+  });
+
 })
